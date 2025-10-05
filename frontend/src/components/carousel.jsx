d3b4954f7d1fddea41cac67bc6c9a56e3ff162a6
William Githinji <146653695+Githinji0@users.noreply.github.com>
2025-10-05 23:52:54 +0300

    Offers Assets

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -1,11 +1,18 @@
 import { OfferCard } from "./OfferCard";
+import image1 from "../assets/images/c1.jpeg";
+import image2 from "../assets/images/c2.jpeg";
+import image3 from "../assets/images/c3.jpeg";
+import image4 from "../assets/images/c4.jpeg";
+import image5 from "../assets/images/c5.jpeg";
 
 
 export const Carousel = () => {
     const offers = [
-        { image: "", title: "Special Offer 1", description: "Get 20% off on your next trip!", price: "$199" },
-        { image: "", title: "Special Offer 2", description: "Get 30% off on your next trip!", price: "$299" },
-        { image: "", title: "Special Offer 3", description: "Get 40% off on your next trip!", price: "$399" },
+        { image: image1, title: "Special Offer 1", description: "Get 20% off on your next trip!", price: "$199" },
+        { image: image2, title: "Special Offer 2", description: "Get 30% off on your next trip!", price: "$299" },
+        { image: image3, title: "Special Offer 3", description: "Get 40% off on your next trip!", price: "$399" },
+        { image: image4, title: "Special Offer 4", description: "Get 50% off on your next trip!", price: "$499" },
+        { image: image5, title: "Special Offer 5", description: "Get 60% off on your next trip!", price: "$599" },
     ];
     return (
         <div>
